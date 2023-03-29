39d24d1ca90ba45078ffca3a7ccf15067d373f7d
Apple <shakeel.ahmed@codingpixel.com>
2023-03-30 00:11:36 +0500

    bug fixes

--- Pods/Target Support Files/ALBusSeatView/ALBusSeatView-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_ALBusSeatView : NSObject
-@end
-@implementation PodsDummy_ALBusSeatView
-@end
--- Pods/Target Support Files/MHWebViewController/MHWebViewController-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_MHWebViewController : NSObject
-@end
-@implementation PodsDummy_MHWebViewController
-@end
