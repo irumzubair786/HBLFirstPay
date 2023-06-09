f7826fc1b0f04a7b910fa0f1db378c1bae76b7e9
Apple <shakeel.ahmed@codingpixel.com>
2023-06-09 10:12:36 +0500

    fixed atm locator screen

--- First Pay/ViewControllers_Updated_Ui/ATMLocator/ATMDetailVC.swift
+++ First Pay/ViewControllers_Updated_Ui/ATMLocator/ATMDetailVC.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ATMDetailVC: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -23,6 +24,18 @@ class ATMDetailVC: UIViewController {
     
 
     
-    @IBOutlet weak var buttonBack: UIButton!
 
+}
+
+extension ATMDetailVC: UITableViewDelegate, UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        return UITableViewCell()
+    }
 }
