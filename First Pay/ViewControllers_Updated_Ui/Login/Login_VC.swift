64f82188a35dd10f62bbae4ec60a10ba6fb141b4
Apple <shakeel.ahmed@codingpixel.com>
2023-06-15 13:12:51 +0500

    cnic verification date fixed

--- First Pay/ViewControllers_Updated_Ui/Login/Login_VC.swift
+++ First Pay/ViewControllers_Updated_Ui/Login/Login_VC.swift
@@ -369,7 +369,7 @@ class Login_VC: BaseClassVC, UITextFieldDelegate  {
         //shakeel
 //        self.navigateToHome()
 //        return
-        if self.loginObj?.data?.customerHomeScreens?.first?.accountDiscrepant ?? "" == "N" {
+        if self.loginObj?.data?.customerHomeScreens?.first?.accountDiscrepant ?? "" == "Y" {
             let storyboard = UIStoryboard(name: "CNICVerification", bundle: nil)
             let vc = storyboard.instantiateViewController(withIdentifier: "CNICVerification") as! CNICVerification
             vc.modalPresentationStyle = .fullScreen
