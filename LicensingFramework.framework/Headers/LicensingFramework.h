ccc31b2ce16a07daf445de328871509d86b1e5fc
IrumButt <irumzubair786@icloud.com>
2023-07-11 16:07:53 +0500

    conflicts

---
+++ LicensingFramework.framework/Headers/LicensingFramework.h
@@ -0,0 +1,19 @@
+//
+//  LicensingFramework.h
+//  LicensingFramework
+//
+//  Created by Rizwan Awan on 01/01/2019.
+//  Copyright © 2019 Unikrew Solutions Private Limited. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LicensingFramework.
+FOUNDATION_EXPORT double LicensingFrameworkVersionNumber;
+
+//! Project version string for LicensingFramework.
+FOUNDATION_EXPORT const unsigned char LicensingFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LicensingFramework/PublicHeader.h>
+
+#import "Licensing.h"
